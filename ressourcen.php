ddeba2c3563cf8148b99e575245f0a6e1e0161ac
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-02-15 23:09:16 +0100

    test fixed categories

--- ressourcen.php
+++ ressourcen.php
@@ -164,18 +164,202 @@ get_header();
 						<small class="english">Choose topic</small>
 					</summary>
 					<ul class="filters">
-						<?php if( have_rows('filters') ): ?>
-							<?php while( have_rows('filters') ): the_row();
-								$category = get_sub_field('category');
-								?>
-								<li class="filter">
-									<input type="checkbox" class="filtercheck" id="<?php the_sub_field('category'); ?>" />
-									<label for="<?php the_sub_field('category'); ?>">
-										<small class="<?php the_sub_field('category'); ?>"><?php the_sub_field('category'); ?></small>
-									</label>
-								</li>
-							<?php endwhile; ?>
-						<?php endif; ?>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="ableismus" />
+							<label for="ableismus">
+								<small class="ableismus german">ableismus</small>
+								<small class="ableismus english">ableism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="antidiskriminierung" />
+							<label for="antidiskriminierung">
+								<small class="antidiskriminierung german">antidiskriminierung</small>
+								<small class="antidiskriminierung english">antidiscrimination</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="antirassismus" />
+							<label for="antirassismus">
+								<small class="antirassismus german">antirassismus</small>
+								<small class="antirassismus english">antiracism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="antisemitismus" />
+							<label for="antisemitismus">
+								<small class="antisemitismus german">antisemitismus</small>
+								<small class="antisemitismus english">antisemitism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="barrierefreiheit" />
+							<label for="barrierefreiheit">
+								<small class="barrierefreiheit german">barrierefreiheit</small>
+								<small class="barrierefreiheit english">accessibility</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="clubkultur" />
+							<label for="clubkultur">
+								<small class="clubkultur german">clubkultur</small>
+								<small class="clubkultur english">clubculture</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="community" />
+							<label for="community">
+								<small class="community german">community</small>
+								<small class="community english">community</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="diversity" />
+							<label for="diversity">
+								<small class="diversity german">diversity</small>
+								<small class="diversity english">diversity</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="electronic_dance_music" />
+							<label for="electronic_dance_music">
+								<small class="electronic_dance_music german">electronic_dance_music</small>
+								<small class="electronic_dance_music english">electronic_dance_music</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="feminismus" />
+							<label for="feminismus">
+								<small class="feminismus german">feminismus</small>
+								<small class="feminismus english">feminism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="gender" />
+							<label for="gender">
+								<small class="gender german">gender</small>
+								<small class="gender english">gender</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="gesundheit" />
+							<label for="gesundheit">
+								<small class="gesundheit german">gesundheit</small>
+								<small class="gesundheit english">health</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="history" />
+							<label for="history">
+								<small class="history german">history</small>
+								<small class="history english">history</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="intersektionalität" />
+							<label for="intersektionalität">
+								<small class="intersektionalität german">intersektionalität</small>
+								<small class="intersektionalität english">intersectionality</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="klima" />
+							<label for="klima">
+								<small class="klima german">klima</small>
+								<small class="klima english">climate</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="masculinities" />
+							<label for="masculinities">
+								<small class="masculinities german">masculinities</small>
+								<small class="masculinities english">masculinities</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="privileg" />
+							<label for="privileg">
+								<small class="privileg german">privileg</small>
+								<small class="privileg english">privilege</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="queer" />
+							<label for="queer">
+								<small class="queer german">queer</small>
+								<small class="queer english">queer</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="rassismus" />
+							<label for="rassismus">
+								<small class="rassismus german">rassismus</small>
+								<small class="rassismus english">racism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="safer_spaces" />
+							<label for="safer_spaces">
+								<small class="safer_spaces german">safer_spaces</small>
+								<small class="safer_spaces english">safer_spaces</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="safer_use" />
+							<label for="safer_use">
+								<small class="safer_use german">safer_use</small>
+								<small class="safer_use english">safer_use</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="sexismus" />
+							<label for="sexismus">
+								<small class="sexismus german">sexismus</small>
+								<small class="sexismus english">sexism</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="sexualisierte_gewalt" />
+							<label for="sexualisierte_gewalt">
+								<small class="sexualisierte_gewalt german">sexualisierte_gewalt</small>
+								<small class="sexualisierte_gewalt english">sexualized_violence</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="solidarität" />
+							<label for="solidarität">
+								<small class="solidarität german">solidarität</small>
+								<small class="solidarität english">solidarity</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="sprache" />
+							<label for="sprache">
+								<small class="sprache german">sprache</small>
+								<small class="sprache english">language</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="trans" />
+							<label for="trans">
+								<small class="trans german">trans</small>
+								<small class="trans english">trans</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="transformative_justice" />
+							<label for="transformative_justice">
+								<small class="transformative_justice german">transformative_justice</small>
+								<small class="transformative_justice english">transformative_justice</small>
+							</label>
+						</li>
+						<li class="filter">
+							<input type="checkbox" class="filtercheck" id="türpolitik" />
+							<label for="türpolitik">
+								<small class="türpolitik german">türpolitik</small>
+								<small class="türpolitik english">door_policy</small>
+							</label>
+						</li>
 					</ul>
 					<button class="applyFilters german">Filtern</button>
 					<button class="applyFilters english">Filter</button>
