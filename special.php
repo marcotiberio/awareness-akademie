36c38dae6a7902ac46a4128823cf52a3e2f5eb52
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-03-24 09:05:37 +0100

    removed google maps where not needed

--- special.php
+++ special.php
@@ -256,22 +256,6 @@ get_header();
 		<?php endif; ?>
 	</main><!-- #main -->
 
-	<div style="display: none;">
-		<?php if( have_rows('locations') ): ?>
-		<div class="acf-map">
-			<?php while ( have_rows('locations') ) : the_row(); 
-				$location = get_sub_field('location');
-				?>
-				<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
-					<h4><?php the_sub_field('title'); ?></h4>
-						<p class="address"><?php echo $location['address']; ?></p>
-					<p><?php the_sub_field('description'); ?></p>
-				</div>
-			<?php endwhile; ?>
-		</div>
-		<?php endif; ?>
-	</div>
-
 <?php
 get_sidebar();
 get_footer();
